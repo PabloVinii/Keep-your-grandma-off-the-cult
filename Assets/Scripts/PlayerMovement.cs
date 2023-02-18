bd805d806bfc380b6fb20e8ff57313133736bb84
PabloVinii <pvinicius006@hotmail.com>
2023-02-18 11:25:07 -0300

    Initial configs

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 7f;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionX = Input.GetAxisRaw("Horizontal");
+        float directionY = Input.GetAxisRaw("Vertical");
+
+        playerDirection = new Vector2(directionX, directionY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(playerDirection.x * playerSpeed, playerDirection.y * playerSpeed);
+    }
+}
