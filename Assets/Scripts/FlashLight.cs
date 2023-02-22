b5e14789dc83021b9bf7c0bbfba7769555faa7f9
PabloVinii <pvinicius006@hotmail.com>
2023-02-22 18:02:56 -0300

    flashlight and light scene

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class FlashLight : MonoBehaviour
 {
     public Transform rotationObject; // objeto que define a rotação
-    public float speedRotation = 1.0f; // velocidade de rotação
+    public float speedRotation = 10.0f; // velocidade de rotação
 
     void Update()
     {
-    Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-    if (direction != Vector2.zero) // verifica se o jogador está se movendo
+        
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (direction != Vector2.zero) // verifica se o jogador está se movendo
         {
             Quaternion desiredRotation = Quaternion.LookRotation(Vector3.forward, direction);
             Quaternion currentRotation = rotationObject.rotation;
             rotationObject.rotation = Quaternion.Slerp(currentRotation, desiredRotation, Time.deltaTime * speedRotation);
         }
     }
+
+    
 }
