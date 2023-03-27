71962c311672ae7cd8a6672a7c887babbe292de7
PabloVinii <pvinicius006@hotmail.com>
2023-03-27 17:24:08 -0300

    item coletavel de quest

--- Assets/Scripts/item/ItemObject.cs
+++ Assets/Scripts/item/ItemObject.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class ItemObject : MonoBehaviour
 {
+    public int questId;
     public ItemData referenceItem;
     private InventorySystem inventory;
-    
+    public GameObject player;
+    public string varYarn = "$";
+    public DialogueRunner dialogueRunner;
+
     private void Start() {
         inventory = FindObjectOfType<InventorySystem>();
     }
@@ -14,13 +19,31 @@ public class ItemObject : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
         {
+            player = other.gameObject;
             PickUpItem();
         }
     }
 
     public void PickUpItem()
-    {
-        inventory.Add(referenceItem);
-        Destroy(gameObject);
+    {     
+        Quest relatedQuest = player.GetComponent<PlayerActions>().GetQuestById(questId);
+        if (relatedQuest != null && relatedQuest.isActive)
+        {
+            // Coleta o item
+            inventory.Add(referenceItem);
+            relatedQuest.goal.ItemCollected();
+
+            // Verifica se a meta da quest foi alcançada
+            if (relatedQuest.goal.isReached())
+            {
+                dialogueRunner.VariableStorage.SetValue(varYarn, true);
+            }
+
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("nenhuma quest relacionada a este item");
+        }     
     }
 }
