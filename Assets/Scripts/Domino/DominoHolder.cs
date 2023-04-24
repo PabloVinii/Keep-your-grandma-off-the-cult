17a6e7238b09f6cd99ed3cc1085116223b4f2114
PabloVinii <pvinicius006@hotmail.com>
2023-04-24 12:15:36 -0300

    prefab button

---
+++ Assets/Scripts/Domino/DominoHolder.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoHolder : MonoBehaviour
+{
+    public DominoPieceData pieceData;
+}
--- Assets/Scripts/Domino/DominoUI.cs
+++ Assets/Scripts/Domino/DominoUI.cs
@@ -8,7 +8,7 @@ public class DominoUI : MonoBehaviour
     public GameObject piecePrefab;
     public Transform piecesParent;
     public List<GameObject> pieceSlots = new List<GameObject>(); // Lista de slots de itens no inventário
-
+    public PlayerPieces pp;
     public void UpdateUI(List<DominoPieceData> playerPieces)
     {
         // Destruir as peças existentes na UI
@@ -23,8 +23,25 @@ public class DominoUI : MonoBehaviour
             GameObject newPiece = Instantiate(piecePrefab, transform);
             pieceSlots.Add(newPiece);
             newPiece.GetComponent<Image>().sprite = piece.sprite;
+
+            // Adiciona o componente DominoPieceDataHolder e define o objeto DominoPieceData nele
+            DominoHolder holder = newPiece.AddComponent<DominoHolder>();
+            holder.pieceData = piece;
         }
     }
+
+    public void OnPieceClick(GameObject pieceObj)
+    {
+        Debug.Log("perto");
+    //     DominoHolder holder = pieceObj.GetComponent<DominoHolder>();
+
+    //     if (holder != null)
+    //     {
+            
+    //         DominoPieceData piece = holder.pieceData;
+    //         pp.PlayPiece(piece, true);
+    //     }
+    // }
 
 }
 
